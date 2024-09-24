bf4f8d92168b7b2427ff06d768e74053e7bb9b56
Wisdom Ogwu <iammadab@gmail.com>
2024-09-24 14:41:16 +0100

    implement prime field

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
 use ark_ff::PrimeField;
-
 struct Ft<T: PrimeField> {
-    inner: T
+    inner: T,
+}
+
+impl<T: PrimeField> From<T> for Ft<T> {
+    fn from(value: T) -> Self {
+        Ft { inner: value }
+    }
+}
+
+impl<T: PrimeField> PrimeField for Ft<T> {
+    type BigInt = T::BigInt;
+    const MODULUS: Self::BigInt = T::MODULUS;
+    const MODULUS_MINUS_ONE_DIV_TWO: Self::BigInt = T::MODULUS_MINUS_ONE_DIV_TWO;
+    const MODULUS_BIT_SIZE: u32 = T::MODULUS_BIT_SIZE;
+    const TRACE: Self::BigInt = T::TRACE;
+    const TRACE_MINUS_ONE_DIV_TWO: Self::BigInt = T::TRACE_MINUS_ONE_DIV_TWO;
+
+    fn from_bigint(repr: Self::BigInt) -> Option<Self> {
+        T::from_bigint(repr).map(|v| v.into())
+    }
+
+    fn into_bigint(self) -> Self::BigInt {
+        self.inner.into_bigint()
+    }
 }
